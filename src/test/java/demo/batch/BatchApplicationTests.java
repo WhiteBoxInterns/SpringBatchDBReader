abfe44541a468321c52a3dcc45a52b7de2c8bfa5
DragosPredi <predi.dragos@gmail.com>
2020-09-07 12:04:47 +0300

    Added test for Paritioner

--- src/test/java/demo/batch/BatchApplicationTests.java
+++ src/test/java/demo/batch/BatchApplicationTests.java
@@ -1,13 +1,33 @@
 package demo.batch;
 
+import demo.batch.infrastructure.ColumnRangePartitioner;
 import org.junit.jupiter.api.Test;
+import org.springframework.batch.item.ExecutionContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class BatchApplicationTests {
-	
+	@Autowired
+	private DataSource dataSource;
+
 	@Test
-	void contextLoads() {
+	void partionerTest() {
+		ColumnRangePartitioner columnRangePartitioner = new ColumnRangePartitioner();
+
+		columnRangePartitioner.setColumn("tconst");
+		columnRangePartitioner.setDataSource(this.dataSource);
+		columnRangePartitioner.setTable("crew");
+
+		Map<String, ExecutionContext> map = columnRangePartitioner.partition(4);
+		System.out.println(map);
+		assertEquals(4, map.size());
+
 	}
 	
 }
