ee19b0cdec5a7e1938abe914ebde32f94b3f15bb
diana <dianagiosan@yahoo.com>
2020-09-08 14:59:02 +0300

    Fixed errors to accomodate reading from crew table.

--- src/main/java/demo/batch/jparepository/RatingsRepository.java
+++ src/main/java/demo/batch/jparepository/RatingsRepository.java
@@ -1,9 +1,10 @@
 package demo.batch.jparepository;
 
+import demo.batch.domain.Crew;
 import demo.batch.domain.Ratings;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RatingsRepository extends JpaRepository<Ratings, Integer> {
+public interface RatingsRepository extends JpaRepository<Crew, Integer> {
 }
